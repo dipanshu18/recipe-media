e34f76204a74f6c6dbbb3595bb9039a001a49217
dipanshu18 <torawanedipanshu@gmail.com>
2024-05-22 14:28:47 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Flex, Heading } from "@aws-amplify/ui-react";
+
+export default function App() {
+  return (
+    <>
+      <Flex>
+        Hello, world!
+        <Heading>Hello,</Heading>
+        <Heading>Meow</Heading>
+      </Flex>
+    </>
+  );
+}
