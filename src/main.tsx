f45d19bec2c8e275ab93053d3597dcdf2192e9c4
dipanshu18 <torawanedipanshu@gmail.com>
2024-05-24 19:31:45 +0530

    Configured auth✨

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import "@aws-amplify/ui-react/styles.css";
 
+import { Amplify } from "aws-amplify";
+import output from "../amplify_outputs.json";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(output);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Authenticator>
+              {({ user, signOut }) => <Home user={user} signOut={signOut} />}
+            </Authenticator>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
